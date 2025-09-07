db4f6f6ba1b71a6b9c3ddb9dc7e7ce7a282efa0c
arashms1994 <arash.ms1994@gmail.com>
2025-09-07 08:43:25 +0330

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "/SitePages/Notifications.aspx",
+  build: {
+    outDir: "dist",
+    assetsDir: ".",
+    rollupOptions: {
+      output: {
+        entryFileNames: `index.js`,
+        chunkFileNames: `chunk.js`,
+        assetFileNames: `index.css`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
