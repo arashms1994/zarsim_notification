fb1a771c77429fde28b9562039cfffe350dcc718
arashms1994 <arash.ms1994@gmail.com>
2025-09-15 16:08:32 +0330

    Feat: Add get files from portal

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,4 +1,4 @@
-import { BASE_URL } from "./base";
+import { BASE_URL, BASE_URL2 } from "./base";
 import { useQuery } from "@tanstack/react-query";
 import type {
   ICashListItem,
@@ -113,7 +113,52 @@ export const useFiles = ({
   });
 };
 
-export async function getAllNotifications(): Promise<INotification[]> {
+export async function getAllPORTALNotifications(): Promise<INotification[]> {
+  const listTitle = "Notification";
+  let items: INotification[] = [];
+
+  let nextUrl:
+    | string
+    | null = `${BASE_URL2}/_api/web/lists/getbytitle('${listTitle}')/items?$orderby=ID desc`;
+
+  try {
+    while (nextUrl) {
+      const res = await fetch(nextUrl, {
+        headers: {
+          Accept: "application/json;odata=verbose",
+          "Content-Type": "application/json;odata=verbose",
+        },
+      });
+
+      if (!res.ok) {
+        const err = await res.text();
+        throw new Error(
+          `خطا در گرفتن آیتم‌های Notification: ${err} (Status: ${res.status})`
+        );
+      }
+
+      const json: { d: { results: INotification[]; __next?: string } } =
+        await res.json();
+
+      const results = json.d?.results;
+      if (!Array.isArray(results)) {
+        throw new Error(
+          "ساختار داده‌ی برگشتی نامعتبر است: results یک آرایه نیست"
+        );
+      }
+
+      items = [...items, ...results];
+      nextUrl = json.d.__next ?? null;
+    }
+
+    return items;
+  } catch (err) {
+    console.error("خطا در دریافت آیتم‌های Cash_List:", err);
+    throw err;
+  }
+}
+
+export async function getAllCRMNotifications(): Promise<INotification[]> {
   const listTitle = "Notification";
   let items: INotification[] = [];
 
@@ -133,7 +178,7 @@ export async function getAllNotifications(): Promise<INotification[]> {
       if (!res.ok) {
         const err = await res.text();
         throw new Error(
-          `خطا در گرفتن آیتم‌های Cash_List: ${err} (Status: ${res.status})`
+          `خطا در گرفتن آیتم‌های Notification: ${err} (Status: ${res.status})`
         );
       }
 
--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -1,11 +1,19 @@
-import { getAllNotifications } from "@/api/getData";
+import { getAllCRMNotifications, getAllPORTALNotifications } from "@/api/getData";
 import type { INotification } from "@/types/type";
 import { useQuery } from "@tanstack/react-query";
 
-export const useNotifications = () => {
+export const useCRMNotifications = () => {
   return useQuery<INotification[], Error>({
-    queryKey: ["notifications"],
-    queryFn: () => getAllNotifications(),
+    queryKey: ["crm-notifications"],
+    queryFn: () => getAllCRMNotifications(),
+    staleTime: 1000,
+  });
+};
+
+export const usePORTALNotifications = () => {
+  return useQuery<INotification[], Error>({
+    queryKey: ["portal-notifications"],
+    queryFn: () => getAllPORTALNotifications(),
     staleTime: 1000,
   });
 };
